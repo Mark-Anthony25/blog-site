f3eb3ebf9bd4c306946b1d47e73125453716d6cf
Mark-Anthony25 <98404531+Mark-Anthony25@users.noreply.github.com>
2025-06-06 16:26:13 +0800

    Inital Commit

---
+++ src/main/java/com/mycoolestapp/blogsitev1/BlogSitev1Application.java
@@ -0,0 +1,13 @@
+package com.mycoolestapp.blogsitev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSitev1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSitev1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycoolestapp/blogsitev1/BlogSitev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycoolestapp.blogsitev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSitev1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
