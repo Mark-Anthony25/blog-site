b4d17963b9ace72b97d3bf034f2c6469adb75cba
Mark-Anthony25 <98404531+Mark-Anthony25@users.noreply.github.com>
2025-06-06 16:29:29 +0800

    Updated project files

--- src/main/java/com/mycoolestapp/blogsitev1/BlogSitev1Application.java
+++ src/main/java/com/mycoolestapp/blogsitev1/BlogSitev1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogSitev1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogSitev1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSitev1Application.class, args);
+    }
 
 }
